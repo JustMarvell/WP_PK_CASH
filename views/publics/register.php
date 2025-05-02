18be2a1f880f666f1a6ddfc7413194a49d68e696
NotMarvel <80026033+JustMarvell@users.noreply.github.com>
2025-05-03 05:20:57 +0800

    feat: update admin, register, and welcome pages with consistent styling and construction messages

--- views/publics/register.php
+++ views/publics/register.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register Page</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 </head>
+<style>
+    body {
+        font-family: 'Poppins', sans-serif;
+        background-color: #f8f9fa;
+        height: 100vh;
+    }
+    h1 {
+        color: #343a40;
+        font-size: 2.5rem;
+        font-weight: 600;
+        text-align: center;
+    }
+    .header {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
 <body>
-    OTW
+    <h1 class='header'>PAGE STILL UNDER CONSTRUCTION</h1>
 </body>
 </html>
