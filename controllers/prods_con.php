befe7dc33049d52eb57b5d45bc9e0a6c2cd0186c
NotMarvel <80026033+JustMarvell@users.noreply.github.com>
2025-05-03 07:00:43 +0800

    update prods_con

--- controllers/prods_con.php
+++ controllers/prods_con.php
@@ -1,6 +1,6 @@
 <?php
     include_once __DIR__ . '/../models/database_connector.php'; // Include database connection file
-    # include_once __DIR__ . '/../models/products.php'; // Include Product model
+    include_once __DIR__ . '/../models/products.php'; // Include Product model
 
     class ProductController {
         private $db_conn;
@@ -8,7 +8,7 @@
 
         public function __construct($db) {
             $this->db_conn = $db; // Initialize database connection
-            # $this->productModel = new Products($db); // Initialize Product model
+            $this->productModel = new Products($db); // Initialize Product model
         }
 
         // Function to get all products
@@ -19,6 +19,36 @@
         // Function to delete product by ID
         public function DeleteProduct($id) {
             return $this->productModel->DeleteProductById($id); // Call method from Product model to delete product by ID
+        }
+
+        // Function to add product
+        public function AddProduct($name, $quantity, $description, $image, $price, $category) {
+            return $this->productModel->AddProduct($name, $quantity, $description, $image, $price, $category); // Call method from Product model to add product
+        }
+
+        // Function to edit product
+        public function EditProduct($id, $name, $quantity, $description, $image, $price, $category) {
+            return $this->productModel->EditProduct($id, $name, $quantity, $description, $image, $price, $category); // Call method from Product model to edit product
+        }
+
+        // Function to get product by ID
+        public function GetProductByID($id) {
+            return $this->productModel->GetProductById($id); // Call method from Product model to get product by ID
+        }
+
+        // Function to upload image
+        public function UploadImage($img) {
+            return $this->productModel->UploadImage($img); // Call method from Product model to upload image
+        }
+
+        // Function to update quantity
+        public function UpdateQuantity($id, $new_qty) {
+            return $this->productModel->UpdateProductQty($id, $new_qty); // Call method from Product model to update quantity
+        }
+
+        // Function to search product
+        public function SearchProduct($search) {
+            return $this->productModel->SearchProduct($search); // Call method from Product model to search product
         }
     }
 ?>
